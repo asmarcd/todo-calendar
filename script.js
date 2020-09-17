868f2ce3a49970956c54e79d947efd1d904e1508
Asmar <asmarcd@gmail.com>
2020-09-17 15:53:35 -0700

    psuedocoding complete in js file

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log(moment().format());
+console.log(moment().format("hA"));
+// Display on https://momentjs.com/docs/#/displaying/ will show you what to add to moment().format() to get what you need to display on the site.
+// each time slot is a row with 3 columns. Create those using jQuery, not in the html
+
+// TODO: Create one row with time, text area, and button using jQ.
+
+// TODO: Show the time on the top of the calenda,r using moment.js, grab the time, and set the text of the p tag with the id of currentDay to be our time from moment.js
+
+// TODO: Create an array to hold the hours ["9am", "10am", "11am",] etc
+
+// TODO: Using a for loop, loop over the hours array and create a div for row, textarea, and a button
+// Here's the started HTML code: 
+/* <div class="row time-block">
+    <div class="col-md-2 hour">
+        "hour"
+    </div>
+    <textarea class="col-md-8 past">
+
+    </textarea>
+    <button class="col-md-2 saveBtn">
+        "Save"
+    </button>
+</div> */
+
+// TODO: Give elements content, and 
+
+// TODO: give elements the class that they need based on time of day (ex. if time is past, give textArea class of past). 
+// How do I compare hours on page to current time in real life? Find that in the moment.js API. Plugging ""hA"" into the moment().format(HERE) will give you the current hour and AM/PM
+
+// TODO: append elements to the row, and then append the row to the page
+
+
+
