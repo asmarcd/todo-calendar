7a99ea74d8ac3367eb550f81b4662f19bfe282df
Asmar <asmarcd@gmail.com>
2020-09-17 15:55:27 -0700

    psuedocoding complete in js file

--- script.js
+++ script.js
@@ -26,9 +26,28 @@ console.log(moment().format("hA"));
 // TODO: Give elements content, and 
 
 // TODO: give elements the class that they need based on time of day (ex. if time is past, give textArea class of past). 
-// How do I compare hours on page to current time in real life? Find that in the moment.js API. Plugging ""hA"" into the moment().format(HERE) will give you the current hour and AM/PM
+
+// How do I compare hours on page to current time in real life? Find that in the moment.js API. Plugging ""hA"" into the moment().format(HERE) will give you the current hour and AM/PM. You can use this to create an if statement.
+
+// using an if statement, we can check using moment.js to see if the hour we're looping over is past the current hour, if true, give it the class of past. Using military time and >< might be the best way to do it. 
 
 // TODO: append elements to the row, and then append the row to the page
 
+// TODO: Create event listener click event for save button
 
+// TODO: Grab the value of the text area and save it to a variable - i need to be able to save only the text from the text area that is in the same row as my button
 
+// TODO: using localStorage.setItem, save the text to local storage
+
+// TODO: retrieve the local storage data using locaStorage.getItem and display it in the correct area that they belong to it.
+
+// Here's the general layout of the code. swap out 'test' for the hour key, and apply a different class to each created row, like .first and .second usinf jQ. You can set the class to be the same as the key/hour using a varaible in the loop, and push the local storage data to that. 
+
+// localStorage.setItem('test', 'test note 1');
+// localStorage.setItem('test2', 'test note 2');
+
+// var textFromLocalStorage1 = localStorage.getItem('test')
+// $(".first").text(textFromLocalStorage);
+
+// var textFromLocalStorage2 = localStorage.getItem('test2')
+// $(".second").text(textFromLocalStorage);
